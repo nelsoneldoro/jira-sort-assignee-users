f125f9148df1483bdf335d2338332f35515f1d01
Nelson Eldoro <nelsoneldoro@gmail.com>
2022-12-21 10:28:03 -0300

    refactor: add content

---
+++ scripts/content.js
@@ -0,0 +1,36 @@
+const showMoreAssigneesBtnId = "assignee-show-more";
+const assigneesDropdownSelector = ".guavb6-1.cJoruE";
+const assigneeSelector = ".guavb6-2.jPYWbI";
+
+const sortByTextContent = (a, b) => a.textContent.localeCompare(b.textContent);
+
+const onSearch = (sortedAssignees) => (e) => {
+  const filter = e.target.value.toLowerCase();
+  sortedAssignees.forEach((e) => {
+    if (e.textContent.toLowerCase().includes(filter)) {
+      e.style.display = "block";
+    } else {
+      e.style.display = "none";
+    }
+  });
+};
+
+document.addEventListener("click", function (e) {
+  if (e.target.id == showMoreAssigneesBtnId) {
+    const assigneesDropdown = document.querySelector(assigneesDropdownSelector);
+
+    if (assigneesDropdown) {
+      const assignees = document.querySelectorAll(assigneeSelector);
+
+      const sortedAssignees = Array.from(assignees).sort(sortByTextContent);
+      sortedAssignees.forEach((e) => assigneesDropdown.appendChild(e));
+
+      const input = document.createElement("input");
+      input.setAttribute("placeholder", "Search assignees");
+      input.setAttribute("class", "css-1eh73bc em6wsuj7");
+      input.addEventListener("input", onSearch(sortedAssignees));
+
+      assigneesDropdown.prepend(input);
+    }
+  }
+});
